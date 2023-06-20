b302b82b79f0547b66b8f86570bfb621809978f5
Adwaith PJ <adwaithleans616@gmail.com>
2023-06-21 02:05:54 +0530

    Version v1.0

--- spam_prediction_main.py
+++ spam_prediction_main.py
@@ -2,13 +2,39 @@ import nbformat as nbf
 from nbconvert.preprocessors import ExecutePreprocessor
 import json
 
-#assigning jupyter notebook path
-notebook_path = 'spam_prediction.ipynb'
-notebook = nbf.read(notebook_path, as_version=4)
+
+def execute_notebook(notebook_path, input_variable):
+    # Read the notebook file
+    with open(notebook_path) as f:
+        nb = nbf.read(f, as_version=4)
+
+    # Modify the input cell
+    input_cell_index = 23  # Index of the input cell in the notebook
+    nb.cells[input_cell_index].source = f"input_value = '{input_variable}'"
+
+    # Execute the notebook
+    ep = ExecutePreprocessor(timeout=600)
+    ep.preprocess(nb, {'metadata': {'path': './'}})
+
+    # Extract the outputs from executed cells
+    outputs = []
+    for cell in nb.cells:
+        if 'outputs' in cell:
+            for output in cell['outputs']:
+                if 'text' in output:
+                    outputs.append(output['text'].strip())
+
+    return outputs
 
 def send_data():
-    input_values = input("Enter the Subject of mail :")
+    notebook_path = 'spam_prediction.ipynb'
+    input_value = input("Enter the Subject of mail :")
+    result_outputs = execute_notebook(notebook_path, input_value)
+    for output in result_outputs:
+        print(output)
+
     
+        
 #input values 
 cond=1
 while(cond==1):
@@ -24,9 +50,15 @@ while(cond==1):
         print("Invalid input. Please enter an integer.")
 
     if(x==1):
-        result = 
+        send_data()
+    if(x==2):
+        send_data()
+    if(x==3):
+        cond=0
+    else:
+        print("Please enter a valid Option")
         
 
 
-input_value1 = input("Enter the mail Subject")
-input_value2 = 
+# input_value1 = input("Enter the mail Subject")
+
